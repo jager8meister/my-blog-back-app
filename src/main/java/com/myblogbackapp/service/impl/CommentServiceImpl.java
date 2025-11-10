a476f42e5818b8a3ba38cec2e5884ce131535df1
?? <??@??.com>
2025-11-10 16:15:28 +0300

    integration tests

--- src/main/java/com/myblogbackapp/service/impl/CommentServiceImpl.java
+++ src/main/java/com/myblogbackapp/service/impl/CommentServiceImpl.java
@@ -22,7 +22,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 @Slf4j
 @RequiredArgsConstructor
 public class CommentServiceImpl implements CommentService {
--- src/main/java/com/myblogbackapp/service/impl/LikeServiceImpl.java
+++ src/main/java/com/myblogbackapp/service/impl/LikeServiceImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 @Slf4j
 @RequiredArgsConstructor
 public class LikeServiceImpl implements LikeService {
